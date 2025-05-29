f9b8a7322a8d9868a00e902f2a07f0d95fe00d0b
dylan-suhr <suhr.d@northeastern.edu>
2025-05-28 21:17:03 -0400

    Initial Commit

---
+++ PhishQS/PhishQSApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhishQSApp.swift
+//  PhishQS
+//
+//  Created by Dylan Suhr on 5/28/25.
+//
+
+import SwiftUI
+
+@main
+struct PhishQSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PhishQSTests/PhishQSTests.swift
@@ -0,0 +1,17 @@
+//
+//  PhishQSTests.swift
+//  PhishQSTests
+//
+//  Created by Dylan Suhr on 5/28/25.
+//
+
+import Testing
+@testable import PhishQS
+
+struct PhishQSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
